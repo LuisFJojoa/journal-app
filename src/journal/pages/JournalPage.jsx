7958454970f71869714d0eec678403826db1236b
Fernando <luis.jojoa.q@gmail.com>
2022-08-16 23:50:06 -0500

    Create float button

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,4 +1,5 @@
-import { Typography } from "@mui/material"
+import { AddOutlined } from "@mui/icons-material"
+import { IconButton, Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
 import { NoteView, NothingSelectedView } from "../views"
 
@@ -7,9 +8,27 @@ export const JournalPage = () => {
     <JournalLayout>
       
 
-      {/* <NothingSelectedView/> */}
+      <NothingSelectedView/>
 
-      <NoteView/>
+      {/* <NoteView/> */}
+
+      <IconButton
+      size='large'
+      sx={{
+        color:'white',
+        backgroundColor: 'error.main',
+        ':hover': {backgroundColor: 'error.main', opacity: 0.9},
+        position: 'fixed',
+        right: 20,
+        bottom: 50
+
+      }}
+      >
+        <AddOutlined 
+        sx={{
+          fontSize: 30
+        }}/>
+      </IconButton>
       
     </JournalLayout>
   )
