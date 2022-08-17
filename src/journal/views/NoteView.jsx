9e61dc76c8c8cc47f00f02cf9862ff5a5fa3441e
Fernando <luis.jojoa.q@gmail.com>
2022-08-16 23:43:36 -0500

    Create ImageGallery component

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,5 +1,6 @@
 import { SaveOutlined } from "@mui/icons-material"
 import { Button, Grid, TextField, Typography } from "@mui/material"
+import { ImageGallery } from "../components/ImageGallery"
 
 export const NoteView = () => {
   return (
@@ -53,6 +54,8 @@ export const NoteView = () => {
           minRows={5}
         />
       </Grid>
+
+      <ImageGallery/>
     </Grid>
   )
 }
