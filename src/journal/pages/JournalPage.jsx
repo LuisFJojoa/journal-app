10dcd51bc17001bf259b0b22aa916f8f7e983e63
Fernando <luis.jojoa.q@gmail.com>
2022-08-16 21:53:01 -0500

    Create JournalPage

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,9 +1,17 @@
 import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant="h1">JournalPage</Typography>
-    </>
+    <JournalLayout>
+      <Typography> 
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla ut laborum, nemo iure, itaque quis eligendi recusandae laboriosam sed est debitis eum sint ipsa corporis ex ab harum hic in?
+      </Typography>
+
+      {/* NothingSelected */}
+
+      {/* NoteView */}
+      
+    </JournalLayout>
   )
 }
