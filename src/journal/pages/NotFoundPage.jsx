2aa62ea3bff52f1ca2592e772291e8db2a3be3f4
Fernando <luis.jojoa.q@gmail.com>
2022-08-16 19:38:11 -0500

    configuration to main and secondary routes

---
+++ src/journal/pages/NotFoundPage.jsx
@@ -0,0 +1,12 @@
+
+export const NotFoundPage = () => {
+
+  return (
+    <>
+      <div className="mt-3">
+        <h1>Not found page inside journalApp</h1>
+        <hr />
+      </div>
+    </>
+  )
+}
